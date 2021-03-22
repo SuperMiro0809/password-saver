11b7927cfe737204b8ebfaab06ebcc8eb4948d1c
DESKTOP-0PPT076\MSI <m.balev@editcreation.com>
2021-03-22 18:17:37 +0200

    Login and Register page design

--- client/src/components/User/Login/Login.js
+++ client/src/components/User/Login/Login.js
@@ -1,8 +1,29 @@
 import './Login.scss';
+import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Login() {
     return (
-        <h1>Login</h1>
+        <div className="Login">
+             <h2>Login</h2>
+            <Form>
+                <FormGroup>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl type="email" className="form-control" />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl type="password" className="form-control" />
+                </FormGroup>
+                <FormGroup className="btn-wrapper">
+                    <Button className="register-btn">Login</Button>
+                    <div className="options">
+                        <Link to="/register" >Don't have an account?</Link>
+                        <Link to="/register" >Forgot password?</Link>
+                    </div>
+                </FormGroup>
+            </Form>
+        </div>
     );
 }
 
--- client/src/components/User/Register/Register.js
+++ client/src/components/User/Register/Register.js
@@ -1,8 +1,30 @@
 import './Register.scss';
+import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Register() {
     return (
-        <h1>Register</h1>
+        <div className="Register">
+             <h2>Register</h2>
+            <Form>
+                <FormGroup>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl type="email" className="form-control" />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl type="password" className="form-control" />
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Repeat password</FormLabel>
+                    <FormControl type="password" className="form-control" />
+                </FormGroup>
+                <FormGroup className="btn-wrapper">
+                    <Button className="register-btn">Register</Button>
+                    <Link to="/login" >Have an account?</Link>
+                </FormGroup>
+            </Form>
+        </div>
     );
 }
 
